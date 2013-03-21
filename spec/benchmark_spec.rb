fd1150b47e613a3fc61b3e3a4477c6f390f630d5
Anthony Cook <anthonymichaelcook@gmail.com>
2013-03-21 03:10:43 -0700

    Adding benchmarks I used for testing various start/exit times.

---
+++ spec/benchmark_spec.rb
@@ -0,0 +1,58 @@
+require_relative 'spec_helper'
+
+require 'benchmark'
+
+spec 'running benchmarks' do
+  true
+end
+
+def self.iterations
+  200
+end
+
+def self.bench name, command
+  print " -- #{name.ljust(11)} :"
+  puts Benchmark.measure {
+    iterations.times do
+      run command
+    end
+  }
+end
+
+file = './tmp/benchmark.sock'
+File.delete(file) if File.exists?(file) && File.socket?(file)
+
+server = UNIXServer.new(file)
+#socket = server.accept
+
+begin
+
+  bash_socket = <<-BASH_SOCKET
+    bash -c "
+      exec 3<> ./tmp/benchmark.sock
+
+      exec 3<&-
+      exec 3>&-
+      exit
+    "
+  BASH_SOCKET
+
+  interpreters = {
+    bash: %q{bash -c 'exit'},
+    bash_socket: bash_socket,
+    perl: %q{perl -e 'exit'},
+    python: %q{python -c 'exit'},
+    node: %q{node -e "process.exit()"},
+    io: %q{io -e 'exit'},
+    coffee: %q{coffee -e "process.exit()"},
+    ruby: %q{ruby -e 'exit'}
+  }
+
+  interpreters.each do |name, command|
+    bench name.to_s, command
+  end
+
+ensure
+  server.close
+  File.delete(file) if File.exists?(file) && File.socket?(file)
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+require 'bundler'
+Bundler.require :test
+
+require 'uspec'
+extend Uspec
+
+def self.run *args
+  output = Struct.new :pid, :stdout, :stderr
+
+  status = Open4.popen4(*args) do |pid, stdin, stdout, stderr|
+    output = output.new pid, stdout.read, stderr.read
+  end
+
+  [status, output]
+end
+
